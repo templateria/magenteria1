a89d2161d78d363782341403fdc69a14bf9d490c
Pedro Padron <pedro@templateria.com>
2016-01-13 03:43:27 -0200

    Preparing for v0.1.1.

--- package.config.php
+++ package.config.php
@@ -9,7 +9,7 @@ return array(
 	//The Magento Connect extension name.  Must be unique on Magento Connect
 	//Has no relation to your code module name.  Will be the Connect extension name
 	'extension_name'         => 'Templateria_NewsletterSubscribe',
-	'extension_version'      => '0.1.0',
+	'extension_version'      => '0.1.1',
 	'skip_version_compare'   => false,
 	'auto_detect_version'    => false,
 	//Where on your local system you'd like to build the files to
@@ -21,7 +21,7 @@ return array(
 	//Magento Connect information fields.
 	'summary'                => 'Plugin para Magento que sincroniza assinantes da Newsletter com uma conta na Templateria.',
 	'description'            => 'Plugin para Magento que sincroniza assinantes da Newsletter com uma conta na Templateria.',
-	'notes'                  => 'Primeiro release.',
+	'notes'                  => 'Passando a usar cURL como padrão para se comunicar com a API da Templateria.',
 	'author_name'            => 'Pedro Padron',
 	'author_user'            => 'ppadron',
 	'author_email'           => 'pedro@templateria.com',
